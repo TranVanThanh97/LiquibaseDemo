97161b51e1b02a4c39c8596fa063ed6954379701
admin <nhatgiaoict@gmail.com>
2021-04-28 16:44:36 +0700

    init

---
+++ src/main/java/demo/liquibase/App.java
@@ -0,0 +1,74 @@
+package demo.liquibase;
+
+import liquibase.Contexts;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.diff.output.DiffOutputControl;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.LiquibaseException;
+import liquibase.integration.commandline.CommandLineUtils;
+import liquibase.resource.ClassLoaderResourceAccessor;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class App {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, LiquibaseException, ParserConfigurationException, IOException {
+
+        int result = LoadConfig.loadAppConfig();
+        Class.forName(LoadConfig.className);
+        String ip = LoadConfig.serverName;
+        int port = LoadConfig.port;
+        String databaseName = LoadConfig.targetDb;
+        String databaseName2 = LoadConfig.referenceDb;
+        String user = LoadConfig.user;
+        String password = LoadConfig.password;
+
+        String filelogMigrate = LoadConfig.changeLogMigrate;
+        Database database = genDatabase(ip, port, databaseName, user, password);
+        Database database2 = genDatabase(ip, port, databaseName2, user, password);
+        if(LoadConfig.future.equalsIgnoreCase("migrate")){
+            migrate(filelogMigrate, database);
+        }else if(LoadConfig.future.equalsIgnoreCase("diff")){
+            diff(database, database2);
+        }
+    }
+
+    private static void diff(Database referenceDatabase, Database targetDatabase) throws LiquibaseException, ParserConfigurationException, IOException {
+//        Liquibase liquibase = new Liquibase("", new FileSystemResourceAccessor(),referenceDatabase);
+//        DiffResult diffResult = liquibase.diff(referenceDatabase, targetDatabase, new CompareControl());
+//        new FileOutputStream("src/main/resources/db.liquibase/changelog_difference.xml").close();
+//        new DiffToChangeLog(diffResult, new DiffOutputControl()).print("src/main/resources/db.liquibase/changelog_difference.xml");
+//        migrate("db.liquibase/changelog_difference.xml", targetDatabase);
+//        new DiffToChangeLog(diffResult, new DiffOutputControl()).print(System.out);
+
+        new FileOutputStream(LoadConfig.changeLogDiff).close();
+        CommandLineUtils.doDiffToChangeLog(LoadConfig.changeLogDiff, targetDatabase, referenceDatabase, new DiffOutputControl(), null, null);
+        File file = new File(LoadConfig.changeLogDiff);
+        if(file.length()==0){
+            System.out.println("File empty");
+            return;
+        }
+//        migrate(LoadConfig.changeLogDiff, referenceDatabase);
+    }
+
+    private static void migrate(String fileLog, Database database) throws LiquibaseException {
+        Liquibase liquibase = new Liquibase(fileLog, new ClassLoaderResourceAccessor(), database);
+        liquibase.update(new Contexts("ver1"));
+    }
+
+    private static Database genDatabase (String ip, int port, String databaseName, String user, String password) throws ClassNotFoundException, SQLException, DatabaseException {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String url = String.format("jdbc:sqlserver://%s:%d;databaseName=%s;user=%s;password=%s", ip, port, databaseName, user, password);
+        Connection connection = DriverManager.getConnection(url);
+        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+        return database;
+    }
+}
---
+++ src/main/java/demo/liquibase/LoadConfig.java
@@ -0,0 +1,54 @@
+package demo.liquibase;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public class LoadConfig {
+    public static String className;
+    public static String user;
+    public static String password;
+    public static int port;
+    public static String serverName;
+    public static String targetDb;
+    public static String referenceDb;
+    public static String changeLogMigrate;
+    public static String changeLogDiff;
+    public static String future;
+
+    public static Integer loadAppConfig() {
+        int result;
+        try {
+            Path path = Paths.get(System.getProperty("user.dir"), "config", "appconfig.properties");
+            File cFile = path.toFile();
+
+            if (cFile.exists() && !cFile.isDirectory()) {
+                FileInputStream inputStream = new FileInputStream(cFile);
+                Properties cProp = new Properties();
+                cProp.load(inputStream);
+                inputStream.close();
+
+                LoadConfig.className = cProp.getProperty("dataSourceClassName", "com.microsoft.sqlserver.jdbc.SQLServerDataSource");
+                LoadConfig.user = cProp.getProperty("dataSource.user", "");
+                LoadConfig.password = cProp.getProperty("dataSource.password", "");
+                LoadConfig.port = Integer.parseInt(cProp.getProperty("dataSource.portNumber", "1433"));
+                LoadConfig.serverName = cProp.getProperty("dataSource.serverName", "");
+                LoadConfig.targetDb = cProp.getProperty("dataSource.databaseName.target", "");
+                LoadConfig.referenceDb = cProp.getProperty("dataSource.databaseName.reference", "");
+                LoadConfig.changeLogMigrate = cProp.getProperty("file.changelog.migrate", "");
+                LoadConfig.changeLogDiff = cProp.getProperty("file.changelog.diff", "");
+                LoadConfig.future = cProp.getProperty("app.process.future","migrate");
+                result = 0;
+            } else {
+                result = 1;
+            }
+        } catch (Exception e) {
+            result = -1;
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
+}
